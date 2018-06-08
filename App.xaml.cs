86b478c4e7c06a32725bc2826db1ceb7977cf393
Kiran Niranjan <kiran.niranjan@symphony.com>
2018-06-08 10:47:26 +0530

    Add support to japanese and english locale languages

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Paragon.Plugins.ScreenCapture;
 using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
 
@@ -10,10 +11,29 @@ namespace ScreenSnippet
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            string[] availableLocale = { "en-US", "ja-JP" };
             // mandatory filename arg - grabbed output will write to this file.
             if (e.Args.Length == 1 && !string.IsNullOrEmpty(e.Args[0]))
             {
+                System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
                 string filename = e.Args[0];
+                SnippingWindow win = new SnippingWindow(filename);
+                win.Show();
+            }
+            else if (e.Args.Length == 2 && !string.IsNullOrEmpty(e.Args[0]) && !string.IsNullOrEmpty(e.Args[1]))
+            {
+                string filename = e.Args[0];
+                string localeTag = e.Args[1];
+
+                // validate the localeTag with the availableLocale
+                if (availableLocale.Contains(localeTag))
+                {
+                    System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(localeTag);
+                }
+                else
+                {
+                    System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+                }
                 SnippingWindow win = new SnippingWindow(filename);
                 win.Show();
             }
